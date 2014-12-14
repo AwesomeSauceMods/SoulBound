1a056f57d371ce2b8672b83e4d256dd4f79bf4c7
gjgfuj <gjgfujb@gmail.com>
2014-12-14 20:47:41 +1100

    Added travis-ci.org building.

---
+++ build.sh
@@ -0,0 +1,12 @@
+mkdir build
+git clone https://github.com/AwesomeSauceMods/AwesomeSauceCore.git build/src
+git clone https://github.com/AwesomeSauceMods/CoolInfoStuff.git
+mv CoolInfoStuff/* build
+cd build
+rm -r OpenAutomation
+mkdir SoulBound
+cd SoulBound
+git clone https://github.com/AwesomeSauceMods/SoulBound.git src
+cd ..
+bash gradlew setupCiWorkspace
+bash gradlew SoulBound:build
